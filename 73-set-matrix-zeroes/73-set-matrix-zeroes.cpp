199abe0b83432c5f60039a3a79afd6e9268357c8
Pankaj Ghaywat <89834576+pankaj0123@users.noreply.github.com>
2022-07-29 14:53:57 +0530

    Time: 29 ms (25.61%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-  for (int i = 0; i < rows; i++) {
-    //checking if 0 is present in the 0th column or not
-    if (matrix[i][0] == 0) col0 = 0;
-    for (int j = 1; j < cols; j++) {
-      if (matrix[i][j] == 0) {
-        matrix[i][0] = 0;
-        matrix[0][j] = 0;
-      }
+         bool isZeroCol = false;
+        bool isZeroRow = false;
+        for (int i = 0; i < matrix.size(); i++) { //check the first column
+            if (matrix[i][0] == 0) {
+                isZeroCol = true;
+                break;
+            } 
+        }
+        for (int i = 0; i < matrix[0].size(); i++) { //check the first row
+            if (matrix[0][i] == 0) {
+                isZeroRow = true;
+                break;
+            } 
+        }
+        for (int i = 1; i < matrix.size(); i++) { //check except the first row and column
+            for (int j = 1; j < matrix[0].size(); j++) 
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+        }
+        for (int i = 1; i < matrix.size(); i++) { //process except the first row and column
+           for (int j = 1; j < matrix[0].size(); j++) 
+               if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                   matrix[i][j] = 0;
+        }
+        if (isZeroCol) { //handle the first column
+            for (int i = 0; i < matrix.size(); i++) 
+                matrix[i][0] = 0;
+        }
+        if (isZeroRow) { //handle the first row
+            for (int i = 0; i < matrix[0].size(); i++) 
+                matrix[0][i] = 0;
+        }
     }
-  }
-  //traversing in the reverse direction and
-  //checking if the row or col has 0 or not
-  //and setting values of matrix accordingly.
-  for (int i = rows - 1; i >= 0; i--) {
-    for (int j = cols - 1; j >= 1; j--) {
-      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-        matrix[i][j] = 0;
-      }
-    }
-      if(col0==0){
-          matrix[i][0]=0;
-      }
-    
-
-  }
-
         
-    }
 };
