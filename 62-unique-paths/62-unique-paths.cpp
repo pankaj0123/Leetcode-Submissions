92681c74b76ae2c7dfeddc044aa52654ec70b045
Pankaj Ghaywat <89834576+pankaj0123@users.noreply.github.com>
2022-08-03 01:35:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.12%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+     
+        int N=n+m-2;
+        int r=m-1;
+        
+        
+        long res=1;
+        
+        for(int i=1;i<=r;i++)
+        {
+            res=res*(N-r+i)/i;
+        }
+        
+        return res;
+        
+        
+    }
+};
