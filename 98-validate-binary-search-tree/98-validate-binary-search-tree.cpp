02ed32b5980cf479f59b795770eb9e013e88b15c
Pankaj Ghaywat <89834576+pankaj0123@users.noreply.github.com>
2022-08-12 01:18:34 +0530

    Time: 18 ms (61.19%), Space: 21.7 MB (70.37%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,14 +11,21 @@
  */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) {
-    return isValidBST(root, NULL, NULL);
-}
-
-    bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
-        if(!root) return true;
-        if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
-            return false;
-        return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+   bool isValidBST(TreeNode* root) {
+        return isValidBST(root,LONG_MIN,LONG_MAX);
+    }
+    bool isValidBST(TreeNode* root, long min, long max){
+        if(root==NULL) return true;
+        
+        //not in range
+        if(root->val >= max || root->val<=min) return false;
+        
+        // check on left, if it's lesser than root->val
+        // check on right, if it's greater than root->val
+        // keep a recursive check on subtree's left & right
+        bool left=isValidBST(root->left, min, root->val);
+        bool right=isValidBST(root->right, root->val,max);
+        
+        return left && right; // if both L & R say it's a BST, then it's BST 
     }
 };
